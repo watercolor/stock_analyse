1172cdf5a70a605a18400d82425c3e8811755fa2
Ning Zhiming <nzm1985@gmail.com>
2015-12-03 23:16:24 +0800

    add week and month updata handle

--- update_data.py
+++ update_data.py
@@ -1,39 +1,43 @@
 # coding=utf-8
 
-import os
-from datetime import date
 from parse_yaml import *
 from sohu_data import *
-from stockelem import *
 from stock_cfg import *
+from util_date import *
+
 
 class update_data:
     basedir = os.getcwd() + os.sep + "stockdata"
+    namedict = {
+        "d": "day",
+        "w": "week",
+        "m": "month"
+    }
 
-    def __init__(self, startdate=None, enddate=None):
-        today = date.today()
+    def __init__(self, startdate=None, enddate=None, period="d"):
         if startdate == None:
-            self.startdate = str(today.year) + str(today.month) + str(today.day - 1)
+            self.startdate = todaystr()
         else:
             self.startdate = startdate
 
         if enddate == None:
-            self.enddate = str(today.year) + str(today.month) + str(today.day)
+            self.enddate = todaystr()
         else:
             self.enddate = enddate
-        pass
+        self.period = period
 
     def update(self, code):
         name = stockarray.getname(code)
         csv_dir = basedir + os.sep + code + '_' + name
         if os.path.exists(csv_dir) == False:
             os.mkdir(csv_dir)
-        csv_file = csv_dir + os.sep + "20151127" + ".csv"
+        csv_file = csv_dir + os.sep + self.namedict[self.period] + ".csv"
         print "Fetch %-6s(%s)" % (name, code)
-        sohudata = SohuData(code, self.startdate, self.enddate)
+        sohudata = SohuData(code, self.startdate, self.enddate, self.period)
         sohudata.fetchdata()
         print "Store data to " + csv_file
         sohudata.store_csv(csv_file)
+
 
 stockarray = StockCode()
 cfg = stock_cfg()
@@ -44,6 +48,10 @@ enddate = cfg.get_enddate()
 if os.path.exists(basedir) == False:
     os.mkdir(basedir)
 
-update_handle = update_data(startdate= enddate, enddate=todaystr())
+day_handle = update_data(startdate=enddate, enddate=todaystr(), period='d')
+week_handle = update_data(startdate=enddate, enddate=todaystr(), period='w')
+month_handle = update_data(startdate=enddate, enddate=todaystr(), period='m')
 for code in stockarray:
-    update_handle.update(code)
+    day_handle.update(code)
+    week_handle.update(code)
+    month_handle.update(code)
