24d7da7a862263156895b11b600bf03ed64f14b5
Ning Zhiming <nzm1985@gmail.com>
2015-12-18 23:57:33 +0800

    tmp commit for ma update method, too complicated, need optimize

--- ma.py
+++ ma.py
@@ -48,18 +48,57 @@ class ma:
                 result.append(round_price)
             self.result_list.append(result)
 
-    def update(self, ma_file, datalist, period='d'):
-        pass
-    #     eachresult = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    #     pop_price = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    #     queue = [[], [], [], [], [], []]
-    #     madata = csvdata(ma_file)
-    #     for ma_num in self.macfg:
-    #         idx = self.ma_idx[ma_num]
-    #         data_queue = queue[idx]
-    #
-    #         data_queue.append(end_price)
-    #         length = len(data_queue)
+    def update(self, ma_file, datafile, datalist, period='d'):
+        first_calc = [False, False, False, False, False, False]
+        ma_result = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]          #this data calc each ma result
+        prev_manum_price = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]   # prev manum data
+        queue = [[], [], [], [], [], []]   # each hold queue for hold prev n data
+        madata = csvdata(ma_file)
+        data = csvdata(datafile)
+        end_price_array = data.get_elem_list_last_n('end_val', self.macfg[-1])
+        end_price_number = len(end_price_array)
+        ma_last_data = madata.read_last()
+        new_ma_result_list = []
+        # build ma need result, includes last ma_number endprice and last ma_result
+        for ma_num in self.macfg:
+            idx = self.ma_idx[ma_num]
+            data_queue = queue[idx]
+            ma_result[idx] = ma_last_data[idx + 1] # +1 for first is date
+
+            if end_price_number < ma_num:
+                last_n_data = map(lambda  x: x[1], end_price_array[-end_price_number:])
+                prev_manum_price[idx] = None
+            else:
+                last_n_data = map(lambda  x: x[1], end_price_array[1-ma_num:])
+                prev_manum_price[idx] = end_price_array[-ma_num][1]
+            data_queue.extend(last_n_data)
+
+        for i, data in enumerate(datalist):
+            datadate = data[0]
+            end_price = float(data[1])
+            for ma_num in self.macfg:
+                idx = self.ma_idx[ma_num]
+                data_queue = queue[idx]
+                data_queue.append(end_price)
+                length = len(data_queue)
+
+                #add a queue to calc ma price
+                if length == ma_num:
+                    if first_calc[idx] == False:
+                        average_price = sum(data_queue)/length
+                        first_calc[idx] = True
+                    else:
+                        # an accelerate method to calc average, keep precision in calc
+                        average_price = ma_result[idx] + (end_price - prev_manum_price[idx]) / ma_num
+                    ma_result[idx] = average_price
+                    prev_manum_price[idx] = data_queue.pop(0)
+            result = [datadate]
+            for i in range(self.ma_number):
+                round_price = round(ma_result[i], 3)
+                result.append(round_price)
+            new_ma_result_list.append(result)
+        # finally add all calc result to db
+        madata.append_data(new_ma_result_list, period)
 
     def store(self, output_file):
         if self.result_list:
